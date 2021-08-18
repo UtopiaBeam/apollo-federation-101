f161e175cd89be65fe5b24dfaf78a6f3c089dca5
Natchapol Srisang <utopiabeam@gmail.com>
2021-08-18 13:57:38 +0700

    feat(review): add field resolvers

--- services/reviews/resolvers.ts
+++ services/reviews/resolvers.ts
@@ -12,4 +12,17 @@ export const resolvers: Resolvers<Context> = {
       return ctx.prisma.review.create({ data: input })
     },
   },
+  Movie: {
+    avgRating: async (parent, _args, ctx) => {
+      const result = await ctx.prisma.review.aggregate({
+        where: { movieId: parent.id },
+        _avg: { rating: true },
+      })
+
+      return result._avg.rating ?? 0
+    },
+    reviews: async (parent, _args, ctx) => {
+      return ctx.prisma.review.findMany({ where: { movieId: parent.id } })
+    },
+  },
 }
